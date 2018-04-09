1a1b77c5ce9d9e2a23108fc66806e8fc045f151a
achimuris <chimuris@gmail.com>
2018-04-09 19:50:39 -0300

    Refactoring - Ejercicio 1
    Utilice un ciclo for en vez de 4 variables.

--- miPrimeraAplicacionDeConsola/miPrimeraAplicacionDeConsola/Program.cs
+++ miPrimeraAplicacionDeConsola/miPrimeraAplicacionDeConsola/Program.cs
@@ -10,35 +10,18 @@ namespace miPrimeraAplicacionDeConsola
         static void Main(string[] args)
         {
 
-            double nota1=0, nota2=0, nota3=0, nota4=0;
+            double nota = 0;
             double promedio = 0;
-            
-            /*
-                int soyUnINT;
-                DateTime soyUnDATE;
-                bool soyUnBOOL;
-                char soyUnCHAR;
-                string soyUnSTRING;
-            */
 
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Escriba la nota " + i.ToString());
+                nota = double.Parse(Console.ReadLine());
+                promedio += nota;
+            }      
 
-           
-
-            Console.WriteLine("Escriba la primera nota: ");
-            nota1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Escriba la segunda nota: ");
-            nota2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Escriba la tercer nota: ");
-            nota3 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Escriba la cuarta nota: ");
-            nota4 = double.Parse(Console.ReadLine());
-
-
-            promedio = (nota1 + nota2 + nota3 + nota4) / 4;
-
+            promedio = promedio / 4;
+       
             Console.WriteLine("El promedio es: " + promedio);
             Console.WriteLine();
             Console.WriteLine("Toque una tecla para salir");
